e2f1bb16bdff1c16f99fc86d53e042eab9e7d1df
Volodymyr Kurov <vokur13@gmail.com>
2023-01-19 17:16:28 +0200

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Наше промежуточное ПО');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact Page</h1>');
 });
 
 app.listen(3000, () => {
