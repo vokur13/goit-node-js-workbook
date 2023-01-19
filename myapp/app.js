37e47219874f1020746db7fba0e4229723fbea2a
Volodymyr Kurov <vokur13@gmail.com>
2023-01-19 18:21:24 +0200

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000;
+
 app.use((req, res, next) => {
   console.log('Наше промежуточное ПО');
   next();
@@ -10,10 +12,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/contact', (req, res) => {
-  res.send('<h1>Contact Page</h1>');
+app.get('/contact/:id', (req, res) => {
+  res.send(`<h1>Contact Page</h1> Parameter: ${req.params.id}`);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
 });
